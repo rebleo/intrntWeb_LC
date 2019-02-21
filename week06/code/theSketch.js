2ee596dbe9fd029dd87971c60236f2d7710c3401
rebleo <rml444@nyu.edu>
2019-02-21 18:53:05 -0500

    week06

---
+++ week06/code/theSketch.js
@@ -0,0 +1,10 @@
+console.log("hello worlds!"); 	
+
+let theYear = ["jan", "feb", "march","april","5","may", "june"];
+
+console.log(typeof theYear);
+console.log(theYear.length);
+
+for (var i = 0; i < theYear.length; i++){
+	console.log(theYear[i]);
+}
