7376f2d9873923a7431ef570de035ee06f7a738e
rebleo <rml444@nyu.edu>
2019-04-04 19:44:31 -0400

    week11

--- week11/code/theServer.js
+++ week11/code/theServer.js
@@ -1,15 +1,52 @@
 const theExpress = require('express');
 const theApp = theExpress();
-// for now we are going to run it from a local port
-// we'll need more when we want to deploy at a url.
-const thePort = 12345;
-theApp.use(theExpress.static('viewer'));
-theApp.listen(thePort, function() {
-  console.log('Example app listening on port 12345')
+const thePort = theApp.listen(12345);
+// for parsing form data
+// will need to install this: https://www.npmjs.com/package/body-parser
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({
+  extended: true
 });
-// Dynamic files
-// theApp.get('/randomfile', function(theRequest, theResponse){
-// 	const fileToSend = "random.txt";
-// 	theResponse.sendfile(fileToSend, {root: './viewer'});
+theApp.use(urlencodedParser);
+//
+// access the local port + serve the site at that url
+theApp.listen(thePort, function() {
+  console.log('Example app listening on port 12345!')
+})
+// serve up static web files in directory
+theApp.use(theExpress.static('viewer'));
+///
+//
+// POST ROUTE
+// theApp.post('/getTheData', function(theRequest, theResponse) {
+//   var textValue = theRequest.body.userName;
+//   // theRequest.send("You submitted: " + textValue);
+//   // theResponse.send("{ " + textValue + " }");
+//   console.log(textValue);
 // });
-theApp.set('view engine', 'esj');
+//
+//
+// GET ROUTE
+theApp.get('/getTheData', function(theRequest, theResponse) {
+  var textValue = theRequest.query.userName;
+  theResponse.send("<P>You submitted:</p> " + textValue);
+  console.log(textValue);
+
+});
+
+//
+// line 28 in the readme
+theApp.get('/getRandomTxt', function(theRequest, theResponse) {
+  var fileToSend = "theRandom.txt";
+  // Files inside "viewer" folder
+  theResponse.sendfile(fileToSend, {
+    root: './viewer'
+  });
+});
+theApp.get('/getAbout', function(theRequest, theResponse) {
+  var fileToSend = "about.html";
+  // Files inside "viewer" folder
+  theResponse.sendfile(fileToSend, {
+    root: './viewer'
+  });
+});
--- week11/code/viewer/potato.js
+++ week11/code/viewer/potato.js
@@ -1,9 +1,12 @@
-// console.log("hello worlds!");
-const theirName = document.querySelector('input');
-const theButton = document.querySelector('#thisAwesomeButton');
-theButton.addEventListener('click', whatHappens);
-// *********    ------------------------- **********
+console.log("i am a great client side javascript thing");
+//
+//
+const theClick = document.querySelector('#thisAwesomeButton');
+theClick.addEventListener('click', whatHappens);
+//
+const theUser = document.querySelector("#theUser");
+console.log(theUser.value);
+
 function whatHappens(theEvent) {
-  console.log("yay!!");
+  console.log("click");
 }
-const nameData = theirName.value;
